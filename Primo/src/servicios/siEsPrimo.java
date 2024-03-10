826d7f80de02cc189fc092e429fa1f638ed4b9c1
HernanSain <sainhernan@gmail.com>
2024-03-10 08:27:45 -0300

    sc

---
+++ Primo/src/servicios/siEsPrimo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servicios;
+
+/**
+ *
+ * @author John
+ */
+public class siEsPrimo {
+    public boolean verSiEsPrimo(int x){
+        boolean primo = false;
+        
+        return primo;
+    }
+    
+}
