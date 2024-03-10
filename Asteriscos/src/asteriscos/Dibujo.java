2f8dcf2451e82051d5dd1ab20e69b9981eedc71b
HernanSain <sainhernan@gmail.com>
2024-03-10 18:45:08 -0300

    asteriscos finish

--- Asteriscos/src/asteriscos/Dibujo.java
+++ Asteriscos/src/asteriscos/Dibujo.java
@@ -1,10 +1,29 @@
 package asteriscos;
+
 public class Dibujo {
-    public void cuadrado(int la){
+
+    public void cuadrado(int la) {
         //first line
         for (int i = 0; i < la; i++) {
-            System.out.print("*");
+            System.out.print("* ");
         }
+        //(la-2) lines
+        System.out.println("");
+
+        for (int i = 1; i < (la - 1); i++) {
+            System.out.print("* ");
+            for (int j = 1; j < (la - 1); j++) {
+                System.out.print("  ");
+            }
+            System.out.print(" *");
+            System.out.println("");
+        }
+        //ultima linea
+//        System.out.println("");
+        for (int i = 0; i < la; i++) {
+            System.out.print("* ");
+        }
+        System.out.println("");
     }
 
 }
