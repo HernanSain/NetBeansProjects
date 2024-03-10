192bcafd7fedd9977c16a404a427461ea161bfc4
HernanSain <sainhernan@gmail.com>
2024-03-10 17:36:01 -0300

    asteriscos1

---
+++ Asteriscos/src/asteriscos/Dibujo.java
@@ -0,0 +1,10 @@
+package asteriscos;
+public class Dibujo {
+    public void cuadrado(int la){
+        //first line
+        for (int i = 0; i < la; i++) {
+            System.out.print("*");
+        }
+    }
+
+}
