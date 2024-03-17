554a91e1b5083ebd93d80c3bf187e670cfa0a22a
HernanSain <sainhernan@gmail.com>
2024-03-17 16:59:06 -0300

    MenuReducido corregido

--- MenuReducido/src/menureducido/MenuReducido.java
+++ MenuReducido/src/menureducido/MenuReducido.java
@@ -13,38 +13,32 @@ import java.util.Scanner;
  */
 public class MenuReducido {
 
-static Scanner leer = new Scanner(System.in);
+    static Scanner leer = new Scanner(System.in);
+
     public static void main(String[] args) {
- int elije = 0;
-        try {
-            while (elije == 0) {
+        int elije = 0;
+        while (elije == 0) {
+            try {
                 System.out.println("1:opcion 1; 2:opcion 2; 3:salir");
                 elije = leer.nextInt();
                 switch (elije) {
                     case 1:
-                        elije = 1;
+                        System.out.println("you choose option one");
+                        elije = 0;
                         break;
                     case 2:
-                        elije = 2;
+                        System.out.println("you choose option two");
+                        elije = 0;
                         break;
                     case 3:
-                        elije = 3;
+                        elije = 1;
                         break;
                 }
+            } catch (Exception e) {
+                System.out.println("must enter an integer idiot!");
+                elije = 0;
+                leer.nextLine();
             }
-        } catch (Exception e) {
-            System.out.println("must enter an integer idiot!");
-            elije=0;
-            leer.nextLine();
-        }
-        switch(elije){
-            case 1:
-                System.out.println("you choose option one");
-                break;
-            case 2:
-                System.out.println("you choose option two");
-                break;
         }
     }
-    
 }
