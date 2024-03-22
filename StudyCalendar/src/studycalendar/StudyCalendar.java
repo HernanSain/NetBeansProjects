d8cf40c9076c145e042528a264db26f03d3f1847
HernanSain <sainhernan@gmail.com>
2024-03-22 06:53:41 -0300

    Calendar

--- StudyCalendar/src/studycalendar/StudyCalendar.java
+++ StudyCalendar/src/studycalendar/StudyCalendar.java
@@ -18,17 +18,19 @@ public class StudyCalendar {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Calendar c = Calendar.getInstance();//intance of objet calendar c with the present time default format
-        System.out.println("objet calendar c "+c);//shows the object calendar c 
-        System.out.println("milliseconds from the epoch: "+c.getTimeInMillis());
-        System.out.println("fecha calendar "+c.getTime());
-        Calendar d = Calendar.getInstance();//new instance o f calendar
-        d.set(2024, 3, 21, 21, 5);
-        System.out.println("nueva fecha "+d.getTime());
-//        c.set(2025, 2, 21, 19, 36, 30);
-//        Date fecha = c.getTime();
-//        System.out.println(fecha);
+        Calendar fc = Calendar.getInstance();//intance of objet calendar c with the present time default format
+        System.out.println("calendar fecha calendario fc " + fc);//shows the object calendar c 
+        System.out.println("milliseconds from the calendar object fc: " + fc.getTimeInMillis());
+                Date fechaCalendario = fc.getTime();
+        System.out.println("fecha Date de calendar " + fechaCalendario);
+        System.out.println("fecha calendario milliseconds "+fechaCalendario.getTime());
 
+////        Calendar d = Calendar.getInstance();//new instance o f calendar
+////        d.set(2025, 3, 21, 21, 5);
+////        System.out.println("nueva fecha " + d.getTime());
+////        Date g = d.getTime();
+////        System.out.println("g "+g);
+////        System.out.println("fecha Date getTime()"+g.getTime());
     }
 
 }
