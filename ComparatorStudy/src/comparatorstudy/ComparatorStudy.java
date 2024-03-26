0c957ecee25c2c5a22a24dae07ac186604ec592a
HernanSain <sainhernan@gmail.com>
2024-03-26 17:25:41 -0300

    Comparator String and int

--- ComparatorStudy/src/comparatorstudy/ComparatorStudy.java
+++ ComparatorStudy/src/comparatorstudy/ComparatorStudy.java
@@ -5,17 +5,52 @@
  */
 package comparatorstudy;
 
+import entities.Gente;
+import java.util.ArrayList;
+//import java.util.Comparator;
+
 /**
  *
  * @author John
  */
 public class ComparatorStudy {
 
-    /**
-     * @param args the command line arguments
-     */
+//    public static Comparator<Gente> cn = (Gente g1, Gente g2) -> g2.getNombre().compareTo(g1.getNombre());
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        ArrayList<Gente> g = new ArrayList<Gente>();
+        Gente t1 = new Gente();
+        t1.setEdad(25);
+        t1.setNombre("Pepe");
+        g.add(t1);
+
+        Gente t2 = new Gente();
+        t2.setEdad(32);
+        t2.setNombre("George");
+        g.add(t2);
+
+        Gente t3 = new Gente();
+        t3.setEdad(28);
+        t3.setNombre("Silvie");
+        g.add(t3);
+
+        Gente t4 = new Gente();
+        t4.setEdad(35);
+        t4.setNombre("Leopoldo");
+        g.add(t4);
+
+        Gente t5 = new Gente();
+        t5.setEdad(35);
+        t5.setNombre("Analia");
+        g.add(t5);
+        
+        ServiComp sc = new ServiComp();
+        sc.ordenaGente(g);
+        
+        System.out.println("");
+        
+        sc.ordenaPorNombre(g);
+
     }
-    
+
 }
