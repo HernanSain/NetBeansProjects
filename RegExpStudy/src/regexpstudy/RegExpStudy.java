33e1bf1e25b6931932663b79e1cdaef6eff3855c
HernanSain <sainhernan@gmail.com>
2024-03-27 16:29:46 -0300

    RegExp Study

---
+++ RegExpStudy/src/regexpstudy/RegExpStudy.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regexpstudy;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author John
+ */
+public class RegExpStudy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("la Concha", Pattern.CASE_INSENSITIVE);
+
+        Matcher matcher = pattern.matcher("me cago en la concha de la lora puta ");
+        System.out.println(matcher.find());
+    }
+
+}
