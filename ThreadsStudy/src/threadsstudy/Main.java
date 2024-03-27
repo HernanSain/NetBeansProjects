13a43eb6a5737098bee3d52379b67eb141821de4
HernanSain <sainhernan@gmail.com>
2024-03-27 18:00:51 -0300

    Thread&RunnableStudy

---
+++ ThreadsStudy/src/threadsstudy/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threadsstudy;
+
+/**
+ *
+ * @author John
+ */
+public class Main extends Thread{
+    public void run(){
+        System.out.println("¡Viva Perón carajo!");
+    }
+    
+}
