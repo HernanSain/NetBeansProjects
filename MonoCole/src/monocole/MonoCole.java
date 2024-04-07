9e90d3fca20228a184c805d4d34c5e5162da44cb
HernanSain <sainhernan@gmail.com>
2024-04-07 06:59:24 -0300

    MonoCole estudiando el fondo

---
+++ MonoCole/src/monocole/MonoCole.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package monocole;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author john
+ */
+public class MonoCole {
+
+    public static Connection con;
+    public static Statement stm;
+    public static ResultSet rst;
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/colegio?zeroDateTimeBehavior=convertToNull");
+            stm = con.createStatement();
+        
+            stm.execute("");
+            rst.next();
+
+        } catch (Exception e) {
+        }
+
+    }
+
+}
